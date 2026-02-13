72e01962c4fa5a2dfe20f148b4f8916dabeb5770
Bryceson Laing <bklaing2@gmail.com>
2026-02-13 16:34:07 -0600

    update styles

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,47 +8,45 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Realms of Resurgence",
+    pageTitleSuffix: " - Realms of Resurgence",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    baseUrl: "bklaing2.github.io/realms-of-resurgence",
+    ignorePatterns: ["[_]*", "**/[_]*", "_Templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Homenaje",
+        body: "Baskervville",
+        code: "Victor Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "hsl(40, 100%, 85.9%)", // background
+          lightgray: "hsla(0, 0%, 25.1%, 0.07)",
+          gray: "hsla(0, 0%, 25.1%, 0.69)",
+          darkgray: "hsla(213.9, 92%, 9.8%, 0.74)", // Note text
+          dark: "#021630e0", // headers, sidebar note titles (not folders), table of contents
+          secondary: "hsla(205.7, 33.9%, 44.5%, 0.72)", // title, folders, links
+          tertiary: "hsl(204.9, 29.3%, 35.5%)", // link hover, selected note in sidebar, highlighting text
+          highlight: "transparent", // link highlight
+          textHighlight: "transparent",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          light: "#09121c", // background
+          lightgray: "#ffffff14",
+          gray: "hsla(0, 0%, 77.6%, 0.69)",
+          darkgray: "hsla(0, 18.6%, 76.9%, 0.97)", // Note text
+          dark: "hsla(40.6, 34%, 80.4%, 0.94)", // headers, sidebar note titles (not folders)
+          secondary: "hsla(38.4, 62.4%, 67.6%, 0.87)", // title, folders, links
+          tertiary: "hsl(38.8, 100%, 50%)", // link hover, selected note in sidebar, highlighting text
+          highlight: "transparent", // link highlight
+          textHighlight: "transparent",
         },
       },
     },
@@ -71,15 +69,14 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
-      Plugin.FolderPage(),
-      Plugin.TagPage(),
+      // Plugin.FolderPage(),
+      // Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
@@ -89,7 +86,7 @@ const config: QuartzConfig = {
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
