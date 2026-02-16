69a8b3906c9c3ee0bca9c4103f1e48b4d2d27780
Bryceson Laing <bklaing2@gmail.com>
2026-02-16 07:11:25 -0600

    disable rendering of file creation date and tag list

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,8 +21,8 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
+    // Component.ContentMeta(),
+    // Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
